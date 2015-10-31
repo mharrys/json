953cb3f0956170cfd12bfcd2d02a7dece5096ca8
Mattias Harrysson <mathar@kth.se>
2015-10-31 22:21:56 +0100

    add json cli

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Text.JSON
+import System.Environment (getArgs)
+import System.IO (hPutStr, stderr, readFile)
+
 main :: IO ()
-main = putStrLn ""
+main = do
+    args <- getArgs
+    case (null args) of
+      True -> hPutStr stderr "json: expecting path to JSON file\n"
+      False -> parse (head args)
+
+parse :: String -> IO ()
+parse path = do
+    result <- parseFile path
+    case result of
+      Left err -> print err
+      Right json -> print json
